01f1dc4ff44fe2703b1d4cfdeb61dbe98c9b8c29
Savolainen Andrei <s2400800@BC-C02GR0BXQ6LRs-MacBook-Air.local>
2024-11-06 13:12:25 +0200

    Added .json and data is passing

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './EmployeeCard.css'
-import '.'
 
 function EmployeeCard(props) {
 
@@ -31,26 +30,30 @@ function EmployeeCard(props) {
   );
 }
 
-// function EmployeeCard() {
+// function EmployeeCard(props) {
 
-//   const [person, setPerson] = useState({
-//     firstname: "Mike",
-//     lastname: "Stevens",  
-//     department: "HR",
-//     title: "Senior HR Specialist",
-//     salary: 5000,
-//   });
+//   const [role, setRole] = useState(props.initialRole);
+
+//   const clickHandler = () => {
+//     console.log("I was clicked");
+//     if (role === "Team Lead") {
+//       setRole (props.initialRole); 
+//     } else {
+//       setRole ("Team Lead"); 
+//     }
+
+//   }
 
 //   return (
 //     <>
 //     <div className="card">
-//       <div className="card-title">{person.firstname}</div>
-//       <div className="card-content">
-//         {person.lastname}.
-//         {person.department}.
-//         {person.title}.
-//         {person.salary}.
-//       </div>
+//       <div className="card-title">{props.name}</div>
+//       <ul className="card-content">
+//         Department: {props.department},
+//         Role: {role},
+//         Salary: {props.salary}
+//       </ul>
+//         <button onClick={clickHandler}>Promote</button>
 //     </div>
 //     </>
 //   );
